efd50e649d7204cf17a2758a13dd9986ac2fff3e
KIMON APOSTOLIDIS <72627645+apostolidiskimon@users.noreply.github.com>
2023-01-14 19:55:54 +0200

    +Navigation Bar -Sound not working

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,37 @@
 import React from "react";
-import "./header.css";
+import "../header/header.css";
+import IMG from "../../Assets/logo2.png";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="containermenu">
+      <ul>
+        <li>
+          <a href="#">My Account</a>
+          <audio id="beep" preload="auto">
+            <source src="audio/beep.mp3"></source>
+          </audio>
+        </li>
+        <li>
+          <a href="#">Game Pass</a>
+        </li>
+        <li>
+          <a href="#">Stuff Picks</a>
+        </li>
+        <li>
+          <a href="#">Browse Categories</a>
+        </li>
+        <li>
+          <a href="#">Sale</a>
+        </li>
+        <li>
+          <input type="text" placeholder="Search.."></input>
+        </li>
+        <li>
+          <img src={IMG} alt="" />
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Header;
